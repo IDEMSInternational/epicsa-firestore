d3640034683fe7356fc0852c9f118035cd7a612b
Georg Osang <geoo89@gmail.com>
2023-09-26 16:56:44 +0200

    Firestore functionality

---
+++ README.md
@@ -0,0 +1,82 @@
+# E-PICSA Data recorder
+
+A chatbot for recording climate data in a database.
+
+There are multiple components:
+
+- The database, storing entered data. We use Google Firestore for this.
+- An API to add new entries and query existing entries. This uses Google Cloud functions.
+- The chatbot flows, making calls to this API via webhooks. This is done in RapidPro.
+
+## Functionality
+
+Query URLs support the following paths:
+
+- `record`: Record a new entry (creates a new entry)
+- `confirm` (uuid): Verify an entry (adds a confirmation_timestamp)
+- `update` (uuid): Obsolete an entry (adds obsoleted_by) and record a new entry (correction)
+- `retrieve` (uuid): Retrieve an entry
+- `list_recent` (station_uuid): Returns recent entries with "station_uuid"
+
+Firestore entries have a uuid and contain
+
+- `contact_uuid`: RapidPro uuid of the contact who submitted the entry
+- `station_name`: Name of the station that the contact is reporting for.
+- `date`: Date of the measurement (child.results.measurement_date)?
+- `submission_timestamp`: Timestamp when the entry was added to the DB
+- `measurement_type`: The type of measurement. Either `t_max` (maximum temperature in Celsius), `t_min` (minimum temperature in Celsius) or `rainfall` (rainfall in mm).
+- `measurement_value`: measurement on the date `date` for the station `station_name`
+- `is_missing`: `True` if the value is `NaN`, `False` otherwise.
+- `is_confirmed`: `True` value has been confirmed, `False` otherwise.
+- `is_obsolete`: `True` if the entry is obsolete (because an updated entry has been submitted), `False` otherwise.
+- `confirmation_timestamp`: Whether the submitter has verified the data (Optional)
+- `obsoleted_by`: UUID of the entry which makes this entry obsolete. This is used if a user updates an entry. This way, we keep a record of changes made. (Optional)
+
+Note: `is_missing`, `is_confirmed` and `is_obsolete` would be redundant in a relational database, but Firebase only allows querying one field at a time for inequality, so we add these entries to make querying easier.
+
+Sanity checks that are done when recording a new entry:
+
+- Does the entry already exist? If yes, don't update, but return UUID of found entry to request confirmation of overwrite.
+- tmax >= tmin? If not, save the value, but return a warning.
+- rainfall >= 0? If not, save the value, but return a warning.
+
+## How to deploy
+
+https://cloud.google.com/functions/docs/deploy
+
+Two options: 
+
+- Either edit the code directly via the UI
+- Or deploy via console (recommended).
+
+### Console deployment
+
+#### First-time setup
+
+Follow these steps:
+- https://cloud.google.com/sdk/docs/install
+- https://cloud.google.com/sdk/docs/initializing
+
+In Firestore:
+
+- Create collection `climate_data`
+- Create an index in this collection where `contact_uuid` and `is_obsolete` are ASC and `submission_timestamp` and `__name__` are DESC.
+
+#### Deploy after code changes
+
+```
+gcloud functions deploy record-climate-data \
+--gen2 \
+--region=europe-west2 \
+--runtime=python311 \
+--source=. \
+--entry-point=serve \
+--trigger-http
+```
+
+Replace the region as appropriate.
+
+## TODOS
+
+- Upload the flows
+- Secure the Cloud function through an Authorization token or password
---
+++ main.py
@@ -0,0 +1,230 @@
+from firebase_admin import firestore
+import firebase_admin
+import functions_framework
+import uuid
+import math
+from datetime import datetime
+
+def validate_request(request_json):
+    request_json['is_missing'] = False
+    request_json['is_obsolete'] = False
+    request_json['is_confirmed'] = False
+
+    required_fields = {
+        "contact_uuid",
+        "station_name",
+        "date",
+        "measurement_type",
+        "measurement_value",
+    }
+
+    for field in required_fields:
+        if field not in request_json:
+            return f'Missing {field}'
+
+    measurement_type = request_json['measurement_type']
+    if measurement_type not in ['rainfall', 't_max', 't_min']:
+        return f'Invalid measurement type "{measurement_type}"'
+
+    return None
+
+def get_uuid():
+    return str(uuid.uuid4())
+
+def write_record(request_json):
+    db = firestore.client()
+    uuid = get_uuid()
+    doc_ref = db.collection(u'climate_data').document(uuid)
+    doc_ref.set(request_json | {'uuid' : uuid})
+    return uuid
+
+def update_record(uuid, request_json):
+    db = firestore.client()
+    doc_ref = db.collection(u'climate_data').document(uuid)
+    if not doc_ref.get().exists:
+        return 'Invalid UUID', 200
+    doc_ref.set(request_json, merge=True)
+    return "Success", 200
+
+def find_records_by_contact(contact_uuid, limit=None):
+    limit = limit or 10
+    db = firestore.client()
+    col_ref = db.collection(u'climate_data')
+    q = col_ref.where(u'contact_uuid', u'==', contact_uuid) \
+               .where(u'is_obsolete', u'==', False) \
+               .order_by('submission_timestamp') \
+               .limit_to_last(limit)
+    return [doc.to_dict() for doc in q.get()][::-1]
+
+def list_recent_entries(request_json):
+    if 'contact_uuid' not in request_json:
+        return {'error' : 'No Contact UUID in request'}, 200
+    limit = request_json.get('limit')
+    try:
+        limit = int(limit)
+    except:
+        limit = None
+    docs = find_records_by_contact(request_json['contact_uuid'], limit)
+    text = ''
+    uuids = []
+    records = []
+    for i, doc in enumerate(docs):
+        measurement_type = doc['measurement_type']
+        value = doc['measurement_value']
+        text += f"{i+1}. {doc['date']}: {measurement_type} = {value}\n"
+        records.append(doc)
+        uuids.append(doc['uuid'])
+    return {'text' : text, 'uuids' : uuids}, 200
+
+def check_existing_records(request_json):
+    measurement_type = request_json['measurement_type']
+    db = firestore.client()
+    col_ref = db.collection(u'climate_data')
+    q = col_ref.where(u'date', u'==', request_json['date']) \
+               .where(u'contact_uuid', u'==', request_json['contact_uuid']) \
+               .where(u'is_obsolete', u'==', False) \
+               .where(u'measurement_type', u'==', measurement_type)
+    docs = q.stream()
+    # If there is a matching record, return it
+    for doc in docs:
+        return doc.to_dict()
+    return None
+
+def check_warnings(request_json):
+    measurement_type = request_json['measurement_type']
+    if request_json['measurement_value'].lower().strip() in ['m', 'na', 'nan', 'missing']:
+        request_json['measurement_value'] = float('nan')
+        request_json['is_missing'] = True
+        return {}
+    try:
+        value = float(request_json['measurement_value'])
+    except:
+        return {'warning' : 'Value is not a number.'}
+
+    if measurement_type == 'rainfall':
+        if value < 0:
+            return {'warning' : 'Rainfall value must be non-negative.'}
+        else:
+            return {}
+
+    if measurement_type == 't_min':
+        other_type = 't_max'
+    else:
+        other_type = 't_min'
+    record = check_existing_records({'date' : request_json['date'], 'contact_uuid' : request_json['contact_uuid'], 'measurement_type' : other_type})
+    if record is None:
+        return {}
+    other_value = float(record['measurement_value'])
+    if math.isnan(other_value):
+        return {}
+    if measurement_type == 't_max' and value < other_value:
+        return {'warning' : f't_max ({value}) < t_min ({other_value}) for this date'}
+    elif measurement_type == 't_min' and value > other_value:
+        return {'warning' : f't_min ({value}) > t_max ({other_value}) for this date'}
+    return {}
+
+def get_or_update_fields(request_json, data_dict):
+    if 'uuid' not in request_json:
+        return {'error' : 'No UUID in request'}, 200
+    uuid = request_json['uuid']
+    db = firestore.client()
+    doc_ref = db.collection(u'climate_data').document(uuid)
+    doc = doc_ref.get()
+    if doc.exists:
+        if data_dict is not None:
+            db.collection(u'climate_data').document(uuid).set(
+                data_dict, merge=True
+            )
+            return {'uuid' : uuid}, 200
+        else:
+            return doc.to_dict(), 200
+    else:
+        return {'error' : f'No entry with UUID {uuid} found.'}, 200
+
+def record_entry(request_json, check_existing=True):
+    '''
+    Check if the request is valid.
+    If so, check if there is already a matching record,
+        and if so, return it.
+    If there is no existing record, write a new record.
+    '''
+    result = validate_request(request_json)
+    if result is not None:
+        return {'error' : result}, 200
+    if check_existing:
+        existing = check_existing_records(request_json)
+        if existing is not None:
+            return existing | {'existing': 'True'}, 200
+    warnings = check_warnings(request_json)
+    uuid = write_record(request_json | {'submission_timestamp' : str(datetime.utcnow())})
+    return {'uuid' : uuid} | warnings, 200
+
+def confirm_entry(request_json):
+    return get_or_update_fields(request_json, {'confirmation_timestamp' : str(datetime.utcnow()), 'is_confirmed' : True})
+
+def get_entry(request_json):
+    return get_or_update_fields(request_json, data_dict=None)
+
+def update_entry(request_json):
+    # This should probably be refactored
+    old_uuid = request_json.pop('uuid')
+    result = record_entry(request_json, check_existing=False)
+    new_uuid = result[0].get('uuid')
+    print(old_uuid, new_uuid)
+    if new_uuid:
+        result2 = get_or_update_fields(request_json | {'uuid' : old_uuid}, {'obsoleted_by' : new_uuid, 'is_obsolete' : True})
+        if 'error' in result2:
+            return result[0] | {'warning' : 'New entry written, but ' + result2['error']}, 200
+        return result
+    else:
+        return result
+
+
+@functions_framework.http
+def serve(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    # request_json = request.get_json(silent=True)
+    # request_args = request.args
+
+    request_json = request.get_json(silent=True)
+    if not request_json:
+        # return 'Hellooo {}!'.format(name)
+        return {'error' : 'No Data'}, 200
+
+    path = request.path.strip('/')
+    if path == 'record':
+        # Returns a json with either an 'error' entry, an 'existing' entry,
+        # or a 'uuid' of the new record
+        return record_entry(request_json)
+    if path == 'confirm':
+        # Request should contain a UUID of an existing entry.
+        # Updates the entry by adding a confirmation timestamp
+        return confirm_entry(request_json)
+    if path == 'update':
+        # Request should contain a UUID of an existing entry.
+        # Updates the entry by adding a obsoleted_by UUID for a new entry that is created.
+        return update_entry(request_json)
+    if path == 'retrieve':
+        # Request should contain a UUID of an existing entry.
+        # Returns the entry
+        return get_entry(request_json)
+    if path == 'list_recent':
+        # Request should contain a UUID of a contact (contact_uuid)
+        # Returns a list of the 10 most recent submissions by that person:
+        # - as text representation
+        # - as a list of uuids of the respective entries
+        # - as a list of the complete records
+        return list_recent_entries(request_json)
+    else:
+        return {'error' : 'Invalid path'}, 200
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
